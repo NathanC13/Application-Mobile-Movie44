f389f4f932d6c0b7fd861db2481323f80f070e56
E205434Y <nathan.collinet@etu.univ-nantes.fr>
2022-04-01 10:00:40 +0200

    ajout page2

--- app/src/main/java/com/example/movie44/MainActivity.java
+++ app/src/main/java/com/example/movie44/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.movie44;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Button b1 = findViewById(R.id.button);
+        Intent i = new Intent(MainActivity.this, Search.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/movie44/Search.java
@@ -0,0 +1,22 @@
+package com.example.movie44;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import  android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_2);
+
+    }
+}
